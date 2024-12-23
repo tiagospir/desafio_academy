63a0c3f9217f37a8b319517d40f3e71c8b8531a0
Tiago Spironello <spir.tiago@gmail.com>
2024-12-23 18:30:04 +0000

    Criado staging stg_salesorderheader, stg_creditcard; intermediate int_pedido_join; e mart fct_pedido

---
+++ models/intermediate/int_pedido_join.sql
@@ -0,0 +1,28 @@
+with
+    stg_creditcard as (
+        select *
+        from {{ ref('stg_creditcard') }}
+    )
+
+    , stg_salesorderheader as (
+        select *
+        from {{ ref('stg_salesorderheader') }}
+    )
+
+    , int_pedido_join as (
+        select
+            p.PK_PEDIDO
+            , p.FK_CLIENTE
+            , p.FK_FUNCIONARIO
+            , p.FK_TERRITORIO
+            , p.DATA_PEDIDO
+            , p.DATA_ENVIO
+            , p.STATUS_PEDIDO
+            , c.tipo_cartao
+        from stg_salesorderheader as p
+        inner join stg_creditcard as c
+            on p.fk_cartao = c.pk_cartao 
+    )
+
+select *
+from int_pedido_join
